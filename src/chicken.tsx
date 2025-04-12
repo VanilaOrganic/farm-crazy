72dbae1ec52a783a01c9a95b4a05450f76326b22
melon <momobp114@gmail.com>
2025-04-12 22:57:15 +0700

    add chicken and cat

---
+++ src/chicken.tsx
@@ -0,0 +1,43 @@
+import { useLoader } from '@react-three/fiber'
+import { useMemo, useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+import { ThreeElements } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/Addons.js'
+import { clone } from 'three/examples/jsm/utils/SkeletonUtils.js'
+
+export function Chicken(props: ThreeElements['group']) {
+  const chickenRef = useRef<THREE.Mesh>(null!)
+  const boxRef = useRef<THREE.Mesh>(null!)
+
+  const gltf = useLoader(GLTFLoader, '/chicken/scene.gltf')
+  const clonedScene = useMemo(() => clone(gltf.scene), [gltf.scene])
+
+  useFrame(() => {
+    // Optional rotation update
+    // groupRef.current.rotation.y += 0.01
+  })
+
+  return (
+    <group {...props}>
+      <primitive
+        object={clonedScene}
+        ref={chickenRef} 
+        scale={0.1}
+        onClick={() => {
+          chickenRef.current.rotation.y += Math.PI * 0.5
+          chickenRef.current.position.y += Math.PI * 0.5
+        }}
+      />
+      <mesh position={[0, 0, 0]} ref={boxRef} 
+        onClick={() => {
+          boxRef.current.rotation.y += Math.PI * 0.5
+          boxRef.current.position.y += Math.PI * 0.5
+        }} 
+      >
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={'orange'} />
+      </mesh>
+    </group>
+  )
+}
