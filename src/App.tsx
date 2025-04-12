249db1c1b47fd5e35509d4ec8c23f82617e4c30d
Thanakrit Rojsakthavorn <thanakrit.ro@arise.tech>
2025-04-13 01:08:44 +0700

    feat: add textures and enhance Chicken component with plane and controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,77 @@
-import { Canvas } from '@react-three/fiber'
-import { Chicken } from './chicken'
-import { UIIA } from './cat';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useTexture } from "@react-three/drei";
+import { Chicken } from "./chicken";
 
 function App() {
+  const planeAtrributes = {
+    width: 20,
+    height: 20,
+    depth: 0.5,
+  };
+
+  const chickenCount = 10;
+
+  const Plane = () => {
+    const [colorMap, normalMap, roughnessMap] = useTexture([
+      "texture/rocky_terrain_diff_1k.jpg",
+      "texture/rocky_terrain_nor_dx_1k.jpg",
+      "texture/rocky_terrain_rough_1k.jpg",
+    ]);
+
+    return (
+      <mesh rotation={[-Math.PI / 2, 0, 0]}>
+        <boxGeometry
+          args={[
+            planeAtrributes.width,
+            planeAtrributes.height,
+            planeAtrributes.depth,
+          ]}
+        />
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          roughnessMap={roughnessMap}
+          roughness={1} // adjust roughness value if needed
+        />
+      </mesh>
+    );
+  };
+
   return (
-    <Canvas style={{
-      width: '100vw',
-      height: '100vh',
-    }} camera={{ position: [10, 10, 0], fov: 75, }}>
+    <Canvas
+      style={{
+        width: "100vw",
+        height: "100vh",
+      }}
+      camera={{ position: [10, 10, 10], fov: 75 }}
+    >
       <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      {/* <Plane /> */}
-      <Chicken position={[0, 0, 0]} />
-      <UIIA position={[0, 0, 10]} />
-      <UIIA position={[0, 0, -10]} />
+      {/* Adding the OrbitControls */}
+      <OrbitControls />
+      {/* Plane at the base */}
+      <Plane />
+      {/* Chicken always on top of the plane */}
+      {Array.from({ length: chickenCount }, (_, i) => (
+        <Chicken
+          key={i}
+          position={[
+            Math.random() * planeAtrributes.width - planeAtrributes.width / 2,
+            planeAtrributes.depth * 2,
+            Math.random() * planeAtrributes.height - planeAtrributes.height / 2,
+          ]}
+        />
+      ))}
+      {/* Keep chicken slightly above the plane */}
     </Canvas>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/chicken.tsx
+++ src/chicken.tsx
@@ -1,43 +1,44 @@
-import { useLoader } from '@react-three/fiber'
-import { useMemo, useRef } from 'react'
-import { useFrame } from '@react-three/fiber'
-import * as THREE from 'three'
-import { ThreeElements } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/Addons.js'
-import { clone } from 'three/examples/jsm/utils/SkeletonUtils.js'
+import { useLoader, Vector3 } from "@react-three/fiber";
+import { useMemo, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import { clone } from "three/examples/jsm/utils/SkeletonUtils.js";
 
-export function Chicken(props: ThreeElements['group']) {
-  const chickenRef = useRef<THREE.Mesh>(null!)
-  const boxRef = useRef<THREE.Mesh>(null!)
+export function Chicken({ position }: { position: Vector3 }) {
+  const chickenRef = useRef<THREE.Mesh>(null!);
+  // const boxRef = useRef<THREE.Mesh>(null!);
 
   const gltf = useLoader(GLTFLoader, "chicken/scene.gltf");
-  const clonedScene = useMemo(() => clone(gltf.scene), [gltf.scene])
+  const clonedScene = useMemo(() => clone(gltf.scene), [gltf.scene]);
 
   useFrame(() => {
     // Optional rotation update
     // groupRef.current.rotation.y += 0.01
-  })
+  });
 
   return (
-    <group {...props}>
+    <group position={position}>
       <primitive
         object={clonedScene}
-        ref={chickenRef} 
+        ref={chickenRef}
         scale={0.1}
         onClick={() => {
-          chickenRef.current.rotation.y += Math.PI * 0.5
-          chickenRef.current.position.y += Math.PI * 0.5
+          chickenRef.current.rotation.y += Math.PI * 0.5;
+          chickenRef.current.position.y += Math.PI * 0.5;
         }}
       />
-      <mesh position={[0, 0, 0]} ref={boxRef} 
+      {/* <mesh
+        position={[0, 0, 0]}
+        ref={boxRef}
         onClick={() => {
-          boxRef.current.rotation.y += Math.PI * 0.5
-          boxRef.current.position.y += Math.PI * 0.5
-        }} 
+          boxRef.current.rotation.y += Math.PI * 0.5;
+          boxRef.current.position.y += Math.PI * 0.5;
+        }}
       >
         <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={'orange'} />
-      </mesh>
+        <meshStandardMaterial color={"orange"} />
+      </mesh> */}
     </group>
-  )
+  );
 }
